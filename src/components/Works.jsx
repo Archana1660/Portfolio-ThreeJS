eda926f0ceeb798861239357c24a6597eb685365
archana1660 <archanamech16@gmail.com>
2023-08-06 09:48:34 +0530

    portfolio-work:Add work card with tag names and add css property.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,12 +10,12 @@ import { fadeIn, textVariant } from '../utils/motion';
 
 const ProjectCard = ({index, name, description, tags, image, source_code_link}) => {
   return <motion.div variants={fadeIn("up","spring",index*0.5,0.75)}>
-    <Tilt options={{ max: 45, scale: 1, speed: 300 }} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+    <Tilt options={{ max: 45, scale: 1, speed: 450 }} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
       <div className="realtive w-full h-[230px]">
         <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' />
-        <div className="absolute inset-0 justify-end card-img_hover">
-          <div onClick={() => window.open(source_code_link, "_blank")} className='balck-gradient w-10 h-10 rounder-full flex justify-center items-center cursor-pointer'>
-            <img src={github} alt="github" className='w-1/2 h1/2 object-contain' />
+        <div className="absolute inset-0 flex justify-end  m-3 card-img_hover">
+          <div onClick={() => window.open(source_code_link, "_blank")} className='black-gradient w-10 h-10 rounder-full flex justify-center items-center cursor-pointer'>
+            <img src={github} alt="github" className='w-1/2 h-1/2 object-contain' />
           </div>
         </div>
       </div>
@@ -24,7 +24,8 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link}) 
         <p className='mt-2 text-secondary text-[14px]'>{ description}</p>
       </div>
       <div className="mt-4 flex flex-wrap gap-2">
-        {tags.map((tag) => <p key={tag.name} className={`text-[14px] ${tag.color}`}>#{ tag.name}</p>)}
+        {tags.map((tag) =>
+          <p key={tag.name} className={`text-[14px] ${tag.color}`}>#{tag.name}</p>)}
 </div>
 </Tilt>
   </motion.div>
